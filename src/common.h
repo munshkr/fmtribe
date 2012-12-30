bbb183511b67cde5a66af898e718f8ee68ebbd15
Damián Silvani <munshkr@gmail.com>
2012-12-30 15:43:34 -0300

    common: Remove boolean typedefs by including stdbool.h

--- src/common.h
+++ src/common.h
@@ -2,16 +2,14 @@
 #define __COMMON_H__
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <time.h>
 
 typedef char* string;
-typedef int bool;
-typedef unsigned char  byte;
-typedef unsigned short word;
-
-#define true  1
-#define false 0
+typedef uint8_t  byte;
+typedef uint16_t word;
 
 #define KEY_ESC 27
 #define USECS_PER_MINUTE (1000000 * 60)
