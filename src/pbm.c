ddef0c0ea299b90a5d14014b6838c962ced429ab
Damián Silvani <munshkr@gmail.com>
2013-01-02 08:32:54 -0300

    pbm: Move "bit to buffer" routine to its own static function

--- src/pbm.c
+++ src/pbm.c
@@ -6,6 +6,8 @@
 
 #define DIV_CEIL(x,y) ((x + y - 1) / y)
 
+static void expand_raw_buffer(const uint8_t* raw_buf, const unsigned int raw_buf_size, const unsigned int pbm_width, uint8_t* buf);
+
 bool read_pbm_file(const char* path, pbm_file_t* pbm)
 {
     FILE *f = fopen(path, "rb");
@@ -44,23 +46,7 @@ bool read_pbm_file(const char* path, pbm_file_t* pbm)
         return false;
     }
 
-    // read raw buffer into output buffer
-    unsigned int i, j, k, x;
-    for (i = 0, j = 0, x = 0; i < raw_buf_size; i++) {
-        uint8_t byte = raw_buf[i];
-        for (k = 0; k < 8; k++, j++, x++) {
-            if (x == pbm->width) {
-                x = 0;
-                break;
-            }
-            if (byte & 0x80) {
-                pbm->buffer[j] = 1;
-            } else {
-                pbm->buffer[j] = 0;
-            }
-            byte = (byte << 1) & 0xff;
-        }
-    }
+    expand_raw_buffer(raw_buf, raw_buf_size, pbm->width, pbm->buffer);
 
     free(raw_buf);
     fclose(f);
@@ -73,5 +59,25 @@ void free_pbm(pbm_file_t* pbm)
     if (pbm->buffer) {
         free(pbm->buffer);
         pbm->buffer = NULL;
+    }
+}
+
+static void expand_raw_buffer(const uint8_t* raw_buf, const unsigned int raw_buf_size, const unsigned int pbm_width, uint8_t* buf)
+{
+    unsigned int i, j, k, x;
+    for (i = 0, j = 0, x = 0; i < raw_buf_size; i++) {
+        uint8_t byte = raw_buf[i];
+        for (k = 0; k < 8; k++, j++, x++) {
+            if (x == pbm_width) {
+                x = 0;
+                break;
+            }
+            if (byte & 0x80) {
+                buf[j] = 1;
+            } else {
+                buf[j] = 0;
+            }
+            byte = (byte << 1) & 0xff;
+        }
     }
 }
